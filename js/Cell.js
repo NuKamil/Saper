4e57b3ec034fc15dafa4aba47280fe77722976e3
Kamil <kamill1983@gmail.com>
2022-11-12 11:51:35 +0100

    Adding class Cell

---
+++ js/Cell.js
@@ -0,0 +1,11 @@
+export class Cell {
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.value = 0;
+        this.isMine = false;
+        this.isReveal = false;
+        this.isFlagged = false;
+        this.selector = `[data-board]`
+    }
+}
